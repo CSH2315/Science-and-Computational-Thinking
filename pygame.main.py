4abdef699d69d7ca78b985a97335268edd1af514
CSH2315 <105338921+CSH2315@users.noreply.github.com>
2022-06-19 18:10:28 +0900

    미션 4 구현
    일부 기능 player.py 로 넘김

--- pygame.main.py
+++ pygame.main.py
@@ -16,6 +16,8 @@ def draw_text(txt, size, pos, color):
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 
+pygame.mixer.init()
+
 pygame.display.set_caption("총알 피하기")
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,14 +25,25 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FPS = 60
 
+# 충돌 횟수 세기
+collisioncount = 0
 
-pygame.mixer.music.load('bgm.wav')
-pygame.mixer.music.play(-1)
 
 bg_image = pygame.image.load('bg.jpg')
 bg_pos = (0, 0)
 bg_goto = (0, 0)
-player = Player(WIDTH/2, HEIGHT/2)
+# player.py에 mixer 도입했음
+player = Player(WIDTH/2, HEIGHT/2, pygame.mixer)
+
+
+# 평상시 = 0
+gamechannel = pygame.mixer.Channel(0)
+# 배경음악을 게임채널에서 재생
+gamechannel.play(pygame.mixer.Sound('bgm.wav'), -1)
+'''
+pygame.mixer.music.load('bgm.wav')
+pygame.mixer.music.play(-1)
+'''
 
 bullet1 = Bullet(0,0,0.5,0.5)
 bullets = []
@@ -99,30 +112,47 @@ while running:
     for b in bullets:
         b.update_and_draw(dt, screen)
 
+    '''
     for b in bullets:
         if collision(b, player):
-            gameover = True
+            # 폭발
+            Exploding = pygame.image.load('explode.jpg')
+            Exploding = pygame.transform.scale(Exploding, (128, 128))
+            screen.blit(Exploding, (player.pos[0] - 64, player.pos[1] - 64))
             # 충돌 효과음
             ExplodeSound = pygame.mixer.Sound("explosion.wav")
             ExplodeSound.play(0, 0, 0)
+            collisioncount += 1
+            if collisioncount < 5:
+                player.invincibility = True
+                player.invincibility_during = 3000
+                player.invincibility_during -= dt
+                if player.invincibility_during < 0:
+                    player.invincibility = False
+            elif collisioncount == 5:
+                gameover = True
+    '''
 
     
 
     if gameover:
-        # 폭발
-        Exploding = pygame.image.load('explode.jpg')
-        Exploding = pygame.transform.scale(Exploding, (128, 128))
-        screen.blit(Exploding, (player.pos[0] - 64, player.pos[1] - 64))
+        '''screen.blit(Exploding, (player.pos[0] - 64, player.pos[1] - 64))'''
         draw_text("GAME OVER", 100, (WIDTH/2 - 300, HEIGHT/2 - 50), (255,255,255))
         txt = f"Time: {score:.3f}, Bullets: {len(bullets)}"
         draw_text(txt, 32, (WIDTH/2 - 150, HEIGHT/2 + 50), (255, 255, 255))
     else:
         score = time.time() - start_time
-        txt = f"Time: {score:.3f}, Bullets: {len(bullets)}"
+        txt = f"Time: {score:.3f}, Bullets: {len(bullets)}, HP = {player.get_hp()}"
         draw_text(txt, 32, (10, 10), (255, 255, 255))
 
+    pygame.display.update()
+
+    if not gameover:
+        for b in bullets:
+            if collision(b, player):
+                if player.hit():
+                    gameover = True
+            
         if time_for_adding_bullets > 1000:
             bullets.append(Bullet(0, rnd.random()*HEIGHT, rnd.random()-0.5, rnd.random()-0.5))
             time_for_adding_bullets -= 1000
-
-    pygame.display.update()
