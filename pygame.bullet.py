dc808a95cf76f18591b554cbc59b0f2c9970c0c6
CSH2315 <105338921+CSH2315@users.noreply.github.com>
2022-06-20 15:41:56 +0900

    미션 7 보완
    killer

--- pygame.bullet.py
+++ pygame.bullet.py
@@ -1,10 +1,15 @@
 from bullets import IronBead
 from bullets import NormalBullet
 from bullets import Canonball
+from bullets import Killer
 from random import randint
 def Bullet(x, y, to_x, to_y):
     rnd = randint(1, 100)
-    if rnd % 3 == 1:
+    if rnd % 44 == 0:
+        return Killer(x, y, to_x, to_y)
+    elif rnd == 4:
+        return Killer(x, y, to_x, to_y)
+    elif rnd % 3 == 1:
         return NormalBullet(x, y, to_x, to_y)
     elif rnd % 3 == 2:
         return IronBead(x, y, to_x, to_y)
