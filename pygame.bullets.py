0a6d7ab6bda3f584f60f809611fc95a4f326b89f
CSH2315 <105338921+CSH2315@users.noreply.github.com>
2022-06-20 14:37:15 +0900

    미션 7 완료
    기존 bullet 코드를 옮겨오고 자식 클래스를 만들어 여러 총알 종류를 구현시킴

---
+++ pygame.bullets.py
@@ -0,0 +1,31 @@
+import pygame
+
+class Bullet:
+    def __init__(self, x, y, to_x, to_y):
+        self._pos = [x, y]
+        self._to = [to_x, to_y]
+        self._radius = 7
+        self._color = (34, 144, 34)
+
+    def update_and_draw(self, dt, screen):
+        width, height = screen.get_size()
+        self._pos[0] = (self._pos[0] + self._to[0] * dt) % width
+        self._pos[1] = (self._pos[1] + self._to[1] * dt) % height
+        pygame.draw.circle(screen, self._color, self._pos, self._radius)
+
+# Bullet을 부모 클래스로 두고 그 아래에 총알의 종류를 자식 클래스로 만든다.
+class IronBead(Bullet):
+    def __init__(self, x, y, to_x, to_y):
+        super().__init__(x, y, to_x, to_y)
+        self._radius = 5
+        self._color = (190, 0, 0)
+
+class NormalBullet(Bullet):
+    def __init__(self, x, y, to_x, to_y):
+        super().__init__(x, y, to_x, to_y)
+
+class Canonball(Bullet):
+    def __init__(self, x, y, to_x, to_y):
+        super().__init__(x, y, to_x, to_y)
+        self._radius = 10
+        self._color = (201, 228, 225)
